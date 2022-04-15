b32c18752a967e43d29f93d5e81ae979233fede4
Isaak Morales <ice.isaak97@gmail.com>
2022-04-14 23:18:13 -0400

    updated port for Heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const routerNotes = require('./routes/notes')
 
 
 //* Helper method for generating unique ids
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 const app = express();
 
 //* Middleware for parsing JSON and urlencoded form data
@@ -23,5 +23,3 @@ app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
 
-
-//TODO connect to Hiroku 
